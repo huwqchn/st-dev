ebfc2bee33b8f8b9217d2cc2bed4dbe05a91f1d5
arch johnson <huwqchn@gmail.com>
2022-12-10 20:36:09 +0800

    add notocoloremoji

--- config.h
+++ config.h
@@ -11,6 +11,7 @@ static char *font2[] = {
 	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
 	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
   "JetBrains Mono Nerd Font:pixelsize=30:antialias=true:autohint=true",
+  "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
